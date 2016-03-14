b6ca329aed783ba355a1a6e918429fa429b1b979
Dominic Fox <dominic.fox@opencredo.com>
2016-03-14 12:26:38 +0000

    Split and collect with tryable

--- src/test/java/com/codepoetics/ambivalence/EithersTest.java
+++ src/test/java/com/codepoetics/ambivalence/EithersTest.java
@@ -2,12 +2,15 @@ package com.codepoetics.ambivalence;
 
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import static com.codepoetics.ambivalence.Eithers.splitAndCollectLeft;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class EithersTest {
 
@@ -35,9 +38,25 @@ public class EithersTest {
                 factory.makeRight(12),
                 factory.makeRight(23),
                 factory.makeLeft("bar")
-        ).collect(Eithers.splitAndCollectLeft(Collectors.joining(",")));
+        ).collect(splitAndCollectLeft(Collectors.joining(",")));
 
         assertThat(result.getLeft(), equalTo("foo,bar"));
         assertThat(result.getRights(), contains(12, 23));
+    }
+
+    @Test
+    public void splitAndCollectWithTryable() {
+        SplitResult<String, IOException> result = Stream.of("a", "bee", "cee", "d")
+                .map(Tryable.of(c -> {
+                    if (c.length() ==1) {
+                        return c;
+                    } else {
+                        throw new IOException("String too long");
+                    }
+                }))
+                .collect(splitAndCollectLeft(Collectors.joining(",")));
+
+        assertThat(result.getLeft(), equalTo("a,d"));
+        assertThat(result.getRights(), hasSize(2));
     }
 }
